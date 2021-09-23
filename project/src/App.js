da314af1e7a55716d2d3d1eb0da7c93a5951bcc1
Ross Mather <ramather96@gmail.com>
2021-09-23 11:53:12 -0400

    fixed router so that it starts on home page

--- project/src/App.js
+++ project/src/App.js
@@ -14,6 +14,9 @@ function App() {
   return (
   <Router>
     <Switch>
+      <Route path="">
+        <LandingPage/>
+      </Route>
       <Route path="/home">
         <LandingPage/>
       </Route>
